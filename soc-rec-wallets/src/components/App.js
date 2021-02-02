6e48b47679e3f2b08b0453555409c4712dbfc64b
Mohammed El Amine Idmoussi <midmoussi@Mohammeds-MBP.mshome.net>
2021-02-02 04:37:43 +0100

    first commit mohammed

--- soc-rec-wallets/src/components/App.js
+++ soc-rec-wallets/src/components/App.js
@@ -2,6 +2,10 @@ import React, {Component} from 'react';
 import './App.css';
 import Web3 from 'web3';
 import Test from '../abis/Test.json';
+import Encryption from './Encryption';
+import Rsa from './Rsa';
+import Wallet from './Wallet';
+
 
 class App extends Component{
   componentWillMount(){
@@ -10,15 +14,25 @@ class App extends Component{
 
   async loadBlockchainData(){
     const web3 = new Web3(window.ethereum)    
-    const network = await web3.eth.net.getNetworkType()
+    // const network = await web3.eth.net.getNetworkType()
     const networkId = await web3.eth.net.getId()
-    const accounts = await web3.eth.getAccounts()
+    // const accounts = await web3.eth.getAccounts()
     const abi = Test.abi;
     const address = Test.networks[networkId].address
     const test = new web3.eth.Contract(abi, address)
     const count = await test.methods.getCount().call()
     //console.log("web3", ethereum)
     console.log("count", count)
+    const increase = await test.methods.increase().call()
+    await test.events.Increment({}, (error, event) => {
+      console.log("event:",event.returnValues)
+    })
+    // Watch for MyEvent
+    // increase.MyEvent().watch((error, result) => {});
+    console.log("increase",increase)
+    console.log("count", count)
+    
+
   }
 
   render(){
@@ -26,6 +40,9 @@ class App extends Component{
       <div>
         <header>Social Recovery Wallet</header>
         <p>text goes here...</p>
+        <button>increase</button>
+        <Wallet />
+        <p>{}</p>
       </div>
     )
   }
--- soc-rec-wallets/src/contracts/Test.sol
+++ soc-rec-wallets/src/contracts/Test.sol
@@ -4,12 +4,19 @@ contract Test{
     uint private count;
     address private owner;
 
+    // events
+    event Increment();
+
+
     constructor() public{
         owner = msg.sender;
     }
 
-    function increase() public {
-        count++;
+    function increase() public returns (uint){
+        count = count + 2;
+        return count;
+        emit Increment();
+
     }
 
     function decrease() public{
@@ -19,6 +26,15 @@ contract Test{
     function getCount() public view returns (uint){
         return count;
     }
+
+    //receive(encrypted seedphrase) push the encrypted word into the  smart contract
+    //choseHolder(number,adresses) chainlink to chose a random holder
+    //sendSeedPhrase() send encrypted seedphrase to holder
+    //getListHolders(adresse , order) return the person adress to the user
+    //storeContactList(adresse , order, randomMasterkey) store the holder contact list on ipfs
+    // encrypt(pubkey,word) general function
+    // earn(holderAdress,amount) send money to aave to earn interest
+    // 
 
     function() payable external{}
 }
